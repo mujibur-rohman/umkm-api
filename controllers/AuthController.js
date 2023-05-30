7190c076bf3c0d5be11a8e1b1ea82913a3c733b7
Mujiburrohman <muji.official06@gmail.com>
2023-05-30 23:48:21 +0700

    register user checkout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 
 exports.registerUser = async (req, res) => {
@@ -26,6 +27,54 @@ exports.registerUser = async (req, res) => {
       isEmailVerified: false,
     });
     res.status(201).json({ message: "Email Berhasil Didaftarkan" });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    // Ambil value dari body
+    const { email, password } = req.body;
+
+    // Cek apakah email sudah terdaftar atau belum
+    const userAvailable = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!userAvailable)
+      return res.status(404).json({ message: "Email belum terdaftar" });
+
+    // Jika user ada maka cek password
+    const match = await argon2.verify(userAvailable.password, password);
+    if (!match) return res.status(400).json({ message: "Password Salah" });
+
+    const accessToken = jwt.sign(
+      {
+        user: {
+          id: userAvailable.id,
+          uuid: userAvailable.uuid,
+          name: userAvailable.name,
+          email: userAvailable.email,
+          profilePicture: userAvailable.profilePicture,
+          role: userAvailable.role,
+          emailVerified: userAvailable.emailVerified,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      id: userAvailable.id,
+      uuid: userAvailable.uuid,
+      name: userAvailable.name,
+      email: userAvailable.email,
+      profilePicture: userAvailable.profilePicture,
+      role: userAvailable.role,
+      emailVerified: userAvailable.emailVerified,
+      token: { accessToken, expired: 60 },
+    });
   } catch (error) {
     res.status(400).json(error.message);
   }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { registerUser } = require("../controllers/AuthController");
+const { registerUser, login } = require("../controllers/AuthController");
 
 const authRoute = express.Router();
 
 authRoute.post("/auth/register", registerUser);
+authRoute.post("/auth/login", login);
 
 module.exports = authRoute;
